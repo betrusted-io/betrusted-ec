5cc0aab8a7651597faef2215d66893b340f48ba5
bunnie <bunnie@kosagi.com>
2019-09-29 00:32:02 +0800

    add build retry script (to try different seeds in case timing doesn't work)

---
+++ retry-build.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import os
+
+seed = 10
+
+for attempt in range(0, 20):
+    print(" =============> Trying with seed " + str(seed))
+    ret = os.system('./betrusted-ec.py --boot-source=spi --seed=' + str(seed))
+    if ret == 0:
+        break
+    seed = seed + 1
+
