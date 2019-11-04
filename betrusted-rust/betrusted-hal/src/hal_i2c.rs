e9b0257fb47b65d1cff8230fd47dd21411f48f2c
bunnie <bunnie@kosagi.com>
2019-11-05 00:14:11 +0800

    change variable naming to be more rust-y
    the manual says that variable names should be as short as possible?

--- betrusted-rust/betrusted-hal/src/hal_i2c.rs
+++ betrusted-rust/betrusted-hal/src/hal_i2c.rs
@@ -1,84 +1,84 @@
 pub mod hal_i2c {
     use crate::hal_time::hal_time::get_time_ms;
 
-    pub fn i2c_init(peripherals: &betrusted_pac::Peripherals, clockmhz: u32) {
+    pub fn i2c_init(p: &betrusted_pac::Peripherals, clockmhz: u32) {
         let clkcode: u32 = (clockmhz * 1_000_000) / (5 * 100_000) - 1;
 
         // set the prescale assuming 100MHz cpu operation: 100MHz / ( 5 * 100kHz ) - 1 = 199
-        unsafe{peripherals.I2C.prescale0.write( |w| {w.bits(clkcode & 0xFF)}); }
-        unsafe{peripherals.I2C.prescale1.write( |w| {w.bits((clkcode >> 8) & 0xFF)}); }
+        unsafe{p.I2C.prescale0.write( |w| {w.bits(clkcode & 0xFF)}); }
+        unsafe{p.I2C.prescale1.write( |w| {w.bits((clkcode >> 8) & 0xFF)}); }
 
         // enable the block
-        peripherals.I2C.control.write( |w| {w.en().bit(true)});
+        p.I2C.control.write( |w| {w.en().bit(true)});
     }
 
-    fn i2c_tip_wait(peripherals: &betrusted_pac::Peripherals, timeout_ms: u32) -> u32 {
-        let starttime: u32 = get_time_ms(peripherals);
+    fn i2c_tip_wait(p: &betrusted_pac::Peripherals, timeout_ms: u32) -> u32 {
+        let starttime: u32 = get_time_ms(p);
 
         // wait for TIP to go high
         loop {
-            if peripherals.I2C.status.read().tip().bit() == true {
+            if p.I2C.status.read().tip().bit() == true {
                 break;
             }
-            if get_time_ms(peripherals) > starttime + timeout_ms {
-                unsafe{peripherals.I2C.command.write( |w| {w.bits(0)}); }
+            if get_time_ms(p) > starttime + timeout_ms {
+                unsafe{p.I2C.command.write( |w| {w.bits(0)}); }
                 return 1;
             }
         }
 
         // wait for tip to go low
         loop {
-            if peripherals.I2C.status.read().tip().bit() == false {
+            if p.I2C.status.read().tip().bit() == false {
                 break;
             }
-            if get_time_ms(peripherals) > starttime + timeout_ms {
-                unsafe{peripherals.I2C.command.write( |w| {w.bits(0)}); }
+            if get_time_ms(p) > starttime + timeout_ms {
+                unsafe{p.I2C.command.write( |w| {w.bits(0)}); }
                 return 1;
             }
         }
-        unsafe{peripherals.I2C.command.write( |w| {w.bits(0)}); }
+        unsafe{p.I2C.command.write( |w| {w.bits(0)}); }
 
         0
     }
 
-    pub fn i2c_master(peripherals: &betrusted_pac::Peripherals, addr: u8, txbuf: &[u8], rxbuf: &mut [u8], timeout_ms: u32) -> u32 {
+    pub fn i2c_master(p: &betrusted_pac::Peripherals, addr: u8, txbuf: &[u8], rxbuf: &mut [u8], timeout_ms: u32) -> u32 {
         let mut ret: u32 = 0;
 
         // write half
         if txbuf.len() > 0 {
-            unsafe{ peripherals.I2C.txr.write( |w| {w.bits( (addr << 1 | 0) as u32 )}); }
-            peripherals.I2C.command.write( |w| {w.sta().bit(true).wr().bit(true)});
+            unsafe{ p.I2C.txr.write( |w| {w.bits( (addr << 1 | 0) as u32 )}); }
+            p.I2C.command.write( |w| {w.sta().bit(true).wr().bit(true)});
 
-            ret += i2c_tip_wait(peripherals, timeout_ms);
+            ret += i2c_tip_wait(p, timeout_ms);
 
             let mut i: usize = 0;
             loop {
                 if i == txbuf.len() as usize {
                     break;
                 }
-                if peripherals.I2C.status.read().rx_ack().bit() {
+                if p.I2C.status.read().rx_ack().bit() {
                     ret += 1;
                 }
-                unsafe{ peripherals.I2C.txr.write( |w| {w.bits( (txbuf[i]) as u32 )}); }
+                unsafe{ p.I2C.txr.write( |w| {w.bits( (txbuf[i]) as u32 )}); }
                 if i == txbuf.len() - 1 && rxbuf.len() == 0 {
-                    peripherals.I2C.command.write( |w| {w.wr().bit(true).sto().bit(true)});
+                    p.I2C.command.write( |w| {w.wr().bit(true).sto().bit(true)});
                 } else {
-                    peripherals.I2C.command.write( |w| {w.wr().bit(true)});
+                    p.I2C.command.write( |w| {w.wr().bit(true)});
                 }
-                ret += i2c_tip_wait(peripherals, timeout_ms);
+                ret += i2c_tip_wait(p, timeout_ms);
                 i += 1;
             }
-            if peripherals.I2C.status.read().rx_ack().bit() {
+            if p.I2C.status.read().rx_ack().bit() {
                 ret += 1;
             }
         }
 
         // read half
         if rxbuf.len() > 0 {
-            unsafe{ peripherals.I2C.txr.write( |w| {w.bits( (addr << 1 | 1) as u32 )}); }
-            peripherals.I2C.command.write( |w| {w.sta().bit(true).wr().bit(true)});
+            unsafe{ p.I2C.txr.write( |w| {w.bits( (addr << 1 | 1) as u32 )}); }
+            p.I2C.command.write( |w| {w.sta().bit(true).wr().bit(true)});
 
-            ret += i2c_tip_wait(peripherals, timeout_ms);
+            ret += i2c_tip_wait(p, timeout_ms);
 
             let mut i: usize = 0;
             loop {
@@ -86,12 +86,12 @@ pub mod hal_i2c {
                     break;
                 }
                 if i == rxbuf.len() - 1 {
-                    peripherals.I2C.command.write( |w| {w.rd().bit(true).ack().bit(true).sto().bit(true)});
+                    p.I2C.command.write( |w| {w.rd().bit(true).ack().bit(true).sto().bit(true)});
                 } else {
-                    peripherals.I2C.command.write( |w| {w.rd().bit(true)});
+                    p.I2C.command.write( |w| {w.rd().bit(true)});
                 }
-                ret += i2c_tip_wait(peripherals, timeout_ms);
-                rxbuf[i] = peripherals.I2C.rxr.read().bits() as u8;
+                ret += i2c_tip_wait(p, timeout_ms);
+                rxbuf[i] = p.I2C.rxr.read().bits() as u8;
                 i += 1;
             }
         }
--- betrusted-rust/src/main.rs
+++ betrusted-rust/src/main.rs
@@ -13,6 +13,7 @@ const BQ24157_ADDR: u8 = 0x6a;
 const BQ24157_ID_ADR: u8 = 3;
 
 // allocate a global, unsafe static string for debug output
+#[used]  // test to see if the "used" attribute means we no longer need to call debugcommit() <<< 
 static mut DBGSTR: [u32; 4] = [0, 0, 0, 0];
 
 #[panic_handler]
@@ -20,10 +21,10 @@ fn panic(_panic: &PanicInfo<'_>) -> ! {
     loop {}
 }
 
-// debug simply forces DBGSTR to be committed to an unsafe 
-fn debugcommit(peripherals: &betrusted_pac::Peripherals) {
+// debug simply forces DBGSTR to be committed to an unsafe bbbbb
+fn debugcommit(p: &betrusted_pac::Peripherals) {
     for i in 0..4 {
-        unsafe{&peripherals.RGB.raw.write( |w| {w.bits(DBGSTR[i] as u32)}); }
+        unsafe{&p.RGB.raw.write( |w| {w.bits(DBGSTR[i] as u32)}); }
     }
 }
 
@@ -32,24 +33,24 @@ fn main() -> ! {
     use betrusted_hal::hal_i2c::hal_i2c::*;
     use betrusted_hal::hal_time::hal_time::*;
 
-    let peripherals = betrusted_pac::Peripherals::take().unwrap();
+    let p = betrusted_pac::Peripherals::take().unwrap();
 
-    i2c_init(&peripherals, CONFIG_CLOCK_FREQUENCY / 1_000_000);
-    time_init(&peripherals);
+    i2c_init(&p, CONFIG_CLOCK_FREQUENCY / 1_000_000);
+    time_init(&p);
 
     // flash an LED!
     loop { 
         let txbuf: [u8; 1] = [BQ24157_ID_ADR];
         let mut rxbuf: [u8; 1] = [0];
 
-        unsafe{peripherals.RGB.raw.write( |w| {w.bits(5)}); }
-        delay_ms(&peripherals, 500);
+        unsafe{p.RGB.raw.write( |w| {w.bits(5)}); }
+        delay_ms(&p, 500);
 
-        i2c_master(&peripherals, BQ24157_ADDR, &txbuf, &mut rxbuf, I2C_TRANS_TIMEOUT);
+        i2c_master(&p, BQ24157_ADDR, &txbuf, &mut rxbuf, I2C_TRANS_TIMEOUT);
         unsafe{ DBGSTR[0] = rxbuf[0] as u32;}
-        debugcommit(&peripherals);
+        debugcommit(&p);
         
-        unsafe{peripherals.RGB.raw.write( |w| {w.bits(0)}); }
-        delay_ms(&peripherals, 500);
+        unsafe{p.RGB.raw.write( |w| {w.bits(0)}); }
+        delay_ms(&p, 500);
     }
 }
