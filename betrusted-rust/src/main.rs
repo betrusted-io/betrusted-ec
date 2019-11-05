cd59589c6afbfcc1e78839ab50ba462b8e50ebc7
bunnie <bunnie@kosagi.com>
2019-11-05 08:10:53 +0800

    update main to use the charger and gasguage APIs.

--- betrusted-rust/src/main.rs
+++ betrusted-rust/src/main.rs
@@ -7,10 +7,6 @@ use riscv_rt::entry;
 extern crate betrusted_hal;
 
 const CONFIG_CLOCK_FREQUENCY: u32 = 12_000_000;
-const I2C_TRANS_TIMEOUT: u32 = 10;
-
-const BQ24157_ADDR: u8 = 0x6a;
-const BQ24157_ID_ADR: u8 = 3;
 
 // allocate a global, unsafe static string for debug output
 #[used]  // test to see if the "used" attribute means we no longer need to call debugcommit() <<< 
@@ -32,25 +28,37 @@ fn debugcommit(p: &betrusted_pac::Peripherals) {
 fn main() -> ! {
     use betrusted_hal::hal_i2c::hal_i2c::*;
     use betrusted_hal::hal_time::hal_time::*;
+    use betrusted_hal::api_gasgauge::api_gasgauge::*;
+    use betrusted_hal::api_charger::api_charger::*;
 
     let p = betrusted_pac::Peripherals::take().unwrap();
 
     i2c_init(&p, CONFIG_CLOCK_FREQUENCY / 1_000_000);
     time_init(&p);
 
+    gg_start(&p);
+    chg_set_safety(&p);
+    chg_set_autoparams(&p);
+    chg_start(&p);
+
+    unsafe{ DBGSTR[0] = gg_device_type(&p) as u32; }
+
     // flash an LED!
     loop { 
-        let txbuf: [u8; 1] = [BQ24157_ID_ADR];
-        let mut rxbuf: [u8; 1] = [0];
-
         unsafe{p.RGB.raw.write( |w| {w.bits(5)}); }
         delay_ms(&p, 500);
 
-        i2c_master(&p, BQ24157_ADDR, Some(&txbuf), Some(&mut rxbuf), I2C_TRANS_TIMEOUT);
-        unsafe{ DBGSTR[0] = rxbuf[0] as u32;}
-        debugcommit(&p);
+        unsafe{ DBGSTR[1] = gg_voltage(&p) as u32; }
+        if chg_is_charging(&p) {
+            unsafe{ DBGSTR[2] = 1;}
+        } else {
+            unsafe{ DBGSTR[2] = 0;}
+        }
+        debugcommit(&p); // check if the "used" attrbute obviates this call
         
         unsafe{p.RGB.raw.write( |w| {w.bits(0)}); }
         delay_ms(&p, 500);
+
+        chg_keepalive_ping(&p);
     }
 }
