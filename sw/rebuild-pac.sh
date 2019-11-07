5b5226c2406f71639e574a887376eaa80cc4ed10
bunnie <bunnie@kosagi.com>
2019-11-07 18:15:46 +0800

    make rebulid-pac a little more standalone

--- sw/rebuild-pac.sh
+++ sw/rebuild-pac.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+if [ ! -e betrusted-pac ]
+then
+    mkdir betrusted-pac
+fi
+
 cd betrusted-pac
 svd2rust --target riscv -i ../../build/software/soc.svd
 rm -rf src; form -i lib.rs -o src/; rm lib.rs
