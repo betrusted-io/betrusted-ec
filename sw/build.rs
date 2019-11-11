5d4613326b19a8f28d3afeffdbb778cb97e4f6bd
bunnie <bunnie@kosagi.com>
2019-11-11 16:45:15 +0800

    maybe build.rs shouldn't be in git? commit auto-changes anyways...

--- sw/build.rs
+++ sw/build.rs
@@ -1,13 +1,15 @@
-use std::{env, fs};
-use std::path::PathBuf;
 use std::io::Write;
+use std::path::PathBuf;
+use std::{env, fs};
 
 fn main() {
     // Put the linker script somewhere the linker can find it
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
     println!("cargo:rustc-link-search={}", out_dir.display());
 
-    fs::File::create(out_dir.join("memory.x")).unwrap()
-        .write_all(include_bytes!("memory.x")).unwrap();
+    fs::File::create(out_dir.join("memory.x"))
+        .unwrap()
+        .write_all(include_bytes!("memory.x"))
+        .unwrap();
     println!("cargo:rerun-if-changed=memory.x");
 }
