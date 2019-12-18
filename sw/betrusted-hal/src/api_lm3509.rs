ff87ace4f6abc04018a2ba34c01e98f266de981e
bunnie <bunnie@kosagi.com>
2019-12-19 02:33:17 +0800

    add backlight driver

---
+++ sw/betrusted-hal/src/api_lm3509.rs
@@ -0,0 +1,75 @@
+#![allow(dead_code)]
+
+use crate::hal_i2c::i2c_master;
+
+const LM3509_ADDR: u8 = 0x36;
+
+const LM3509_GP_ADR: u8 = 0x10;
+const LM3509_BMAIN_ADR: u8 = 0xA0;
+const LM3509_BSUB_ADR: u8 = 0xB0;
+const LM3509_GPIO_ADR: u8 = 0x80;
+
+const BL_TIMEOUT_MS: u32 = 5;
+
+pub struct BtBacklight {
+    /// number from 0-3 which specifies how fast the brightness level changes (0 is fastest, 3 is slowest)
+    rate_of_change: u8,
+}
+
+impl BtBacklight {
+    pub fn new() -> Self {
+        BtBacklight {
+            rate_of_change: 3,
+        }
+    }
+
+    pub fn set_rate_of_change(&mut self, roc: u8) {
+        let mut roc_local: u8 = roc;
+        if roc_local > 3 {
+            roc_local = 3;
+        }
+        
+        self.rate_of_change = roc_local;
+    }
+
+    pub fn max_brightness() -> u8 {
+        31 as u8
+    }
+
+    pub fn set_brightness(&mut self, p: &betrusted_pac::Peripherals, level: u8) {
+        let mut level_local: u8 = level;
+
+        // turn on main, sub, and unison mode
+        let mut txbuf: [u8; 2] = [LM3509_GP_ADR, 0xC7];
+
+        if level == 0 {
+            // first set the brightness control to 0
+            txbuf[0] = LM3509_BMAIN_ADR;
+            txbuf[1] = level | 0xE0;
+            i2c_master(p, LM3509_ADDR, Some(&txbuf), None, BL_TIMEOUT_MS);
+    
+            // then put the string into shutdown mode
+            txbuf[0] = LM3509_GP_ADR;
+            txbuf[1] = 0xC0 | ((self.rate_of_change & 0x3) << 3);
+            i2c_master(p, LM3509_ADDR, Some(&txbuf), None, BL_TIMEOUT_MS);
+
+            return
+        } else {
+            // activate BMAIN, BSUB and set ramp value
+            txbuf[1] = ((self.rate_of_change & 0x3) << 3) | 0xC7;
+            i2c_master(p, LM3509_ADDR, Some(&txbuf), None, BL_TIMEOUT_MS);
+
+            // clamp brightness level to 31
+            if level_local > 31 {
+                level_local = 31;
+            }
+
+            txbuf[0] = LM3509_BMAIN_ADR;
+            txbuf[1] = level_local | 0xE0;
+
+            i2c_master(p, LM3509_ADDR, Some(&txbuf), None, BL_TIMEOUT_MS);
+        }
+    }
+
+}
+
--- sw/betrusted-hal/src/lib.rs
+++ sw/betrusted-hal/src/lib.rs
@@ -4,6 +4,7 @@ pub mod hal_i2c;
 pub mod hal_time;
 pub mod api_gasgauge;
 pub mod api_charger;
+pub mod api_lm3509;
 
 #[cfg(test)]
 mod tests {
