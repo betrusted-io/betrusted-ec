92f78a8ffe5da958a13dc6594e1fc045bcffcfcd
bunnie <bunnie@kosagi.com>
2020-02-18 00:23:48 +0800

    relocate code to run out of RAM, so it is faster. much faster.

--- bin/rust-rom.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-riscv64-unknown-elf-objcopy -O binary ../sw/target/riscv32i-unknown-none-elf/release/betrusted-ec /tmp/betrusted-ec.bin
-cat ../build/gateware/top_pad.bin /tmp/betrusted-ec.bin > /tmp/bt-ec.bin
---
+++ sw/rust-rom.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+riscv64-unknown-elf-as -fpic loader.S -o loader.elf
+riscv64-unknown-elf-objcopy -O binary loader.elf /tmp/loader.bin
+dd if=/dev/null of=/tmp/loader.bin bs=1 count=1 seek=4096
+
+riscv64-unknown-elf-objcopy -O binary ../sw/target/riscv32i-unknown-none-elf/release/betrusted-ec /tmp/betrusted-ec.bin
+cat ../build/gateware/top_pad.bin /tmp/loader.bin /tmp/betrusted-ec.bin > /tmp/bt-ec.bin
