c6055ccacf7c2384ea11b178e1efdf16f50a8ed6
bunnie <bunnie@kosagi.com>
2020-02-20 17:53:54 +0800

    commit monkey patch script for generating wfx rust bindings
    the script also prints out a doc note with the rationale of
    why we have it, and some important use caveats

---
+++ sw/wfx-binding-generate.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+echo "This script generates the wfx Rust bindings from reference .h files provided by the vendor."
+echo "Note that bindgen is invoked in the x86_64 environment, so technically the bindings are for a different target."
+echo "However, bindgen does not seem to currently run with riscv as a target, so this is the best we can do for now."
+echo "This command should be re-run anytime the imports/wfx submodules are updated, and all structures"
+echo "should be reviewed with the caveat that the data types are sized for x86_64, and not riscv; however, in"
+echo "theory, for the structures involved here these should be a match."
+
+bindgen imports/wfx.h -- -Iimports/wfx-fullMAC-tools/wfx-fullMAC-driver/wfx_fmac_driver -Iimports/wfx-fullMAC-tools/wfx-fullMAC-driver/wfx_fmac_driver/secure_links -Iimports/wfx-fullMAC-tools/wfx-fullMAC-driver/wfx_fmac_driver/bus -Iimports/wfx-fullMAC-tools/wfx-fullMAC-driver/wfx_fmac_driver/firmware -Iimports/wfx-fullMAC-tools/wfx-fullMAC-driver/wfx_fmac_driver/firmware/3.3.1 > betrusted-hal/src/wfx-bindings.rs
+
