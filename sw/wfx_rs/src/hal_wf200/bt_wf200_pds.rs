1adbb4a1167334a50c9c36ae5f34c67ad1101ae4
bunnie <bunnie@kosagi.com>
2020-02-26 03:38:14 +0800

    also commit the output of the updated script

--- sw/wfx_rs/src/hal_wf200/bt_wf200_pds.rs
+++ sw/wfx_rs/src/hal_wf200/bt_wf200_pds.rs
@@ -1,9 +1,29 @@
+    /* AUTOMATICALLY GENERATED -- DO NOT EDIT BY HAND */
+    /*
+     * Copyright 2018, Silicon Laboratories Inc.  All rights reserved.
+     *
+     * Licensed under the Apache License, Version 2.0 (the "License");
+     * you may not use this file except in compliance with the License.
+     * You may obtain a copy of the License at
+     *
+     *     http://www.apache.org/licenses/LICENSE-2.0
+     *
+     * Unless required by applicable law or agreed to in writing, software
+     * distributed under the License is distributed on an "AS IS" BASIS,
+     * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+     * See the License for the specific language governing permissions and
+     * limitations under the License.
+     *
+     */
+
 /// PDS (platform data set) is SiLab's solution to configuring the wifi chip.
 /// Options such as GPIO, PA power, number of antennae are configured with PDS.
-/// The data set here is "compiled" using a script (wfx-pds-compress.py) that
-/// is forked from the SiLabs reference:
-/// https://github.com/SiliconLabs/wfx-fullMAC-tools/blob/80e058089aa788cd8546363733890d87ab81a1c6/Tools/pds_compress
-/// The fork adds this Rust data structure compatibility.
+/// The data set here is "compiled" using a script (pds_compress) that
+/// can be found in SiliconLabs github repository:
+/// https://github.com/SiliconLabs/wfx-linux-tools/blob/master/pds_compress
+/// 
+/// The output is meant to be redirected into a .rs file in the same crate as your
+/// hal driver, and pulled in with the appropriate mod/use statements.
 
 pub const PDS_DATA: [&[u8]; 7] = [
     b"{a:{a:4,b:0}}\0", 
