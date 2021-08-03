07131072054ca45ede51fcf8f02131d841564304
Sam Blenny <68084116+samblenny@users.noreply.github.com>
2021-08-03 13:55:15 +0000

    Use com_rs::serdes::StringDes for wlan ssid & pass
    Switch to using the new com_rs::serdes::StringDes struct for
    deserializing the COM protocol arguments to WLAN_SET_SSID and
    WLAN_SET_PASS. The point is to reduce the amount of ad hoc,
    untested packing and unpacking.
    
    Signed-off-by: samblenny

--- sw/src/wlan.rs
+++ sw/src/wlan.rs
@@ -1,14 +1,8 @@
 use crate::com_bus::com_rx;
-
-// These constants help with reading the right number of u16 words from the COM
-// bus for COM verbs that take string arguments. Strings here are assumed to be
-// variable length, in a fixed size buffer (size varies by verb), and the first
-// word holds the number of bytes in the string (can be null terminated or not).
-//
-const SSID_COM_RX_WORDS: usize = 17; // 1 length + 16 data (max lentth 32 bytes)
-const PASS_COM_RX_WORDS: usize = 33; // 1 length + 32 data (max length 64 bytes)
-pub const SSID_BUF_SIZE: usize = (SSID_COM_RX_WORDS - 1) * 2;
-pub const PASS_BUF_SIZE: usize = (PASS_COM_RX_WORDS - 1) * 2;
+use com_rs::serdes::{
+    SerdesError, StringDes, STR_32_U8_SIZE, STR_32_WORDS, STR_64_U8_SIZE, STR_64_WORDS,
+};
+use com_rs::ComState;
 
 /// Error codes related to COM bus protocol
 pub enum WlanError {
@@ -19,89 +13,71 @@ pub enum WlanError {
 }
 
 pub struct WlanState {
-    pass_len: u16,
-    ssid_len: u16,
-    pass_buf: [u8; PASS_BUF_SIZE],
-    ssid_buf: [u8; SSID_BUF_SIZE],
+    pass_: StringDes<STR_64_WORDS, STR_64_U8_SIZE>,
+    ssid_: StringDes<STR_32_WORDS, STR_32_U8_SIZE>,
 }
 
 impl WlanState {
     pub fn new() -> Self {
         Self {
-            pass_len: 0,
-            ssid_len: 0,
-            pass_buf: [0; PASS_BUF_SIZE],
-            ssid_buf: [0; SSID_BUF_SIZE],
+            pass_: StringDes::<STR_64_WORDS, STR_64_U8_SIZE>::new(),
+            ssid_: StringDes::<STR_32_WORDS, STR_32_U8_SIZE>::new(),
         }
     }
 
-    /// Make a string slice for the SSID (doing it this way helps pacify borrow checker).
+    /// Make a string slice for the SSID
     pub fn ssid(&self) -> Result<&str, WlanError> {
-        if self.ssid_len as usize > SSID_BUF_SIZE {
-            // Looks like an out of range string length argument came over the COM bus
-            return Err(WlanError::StrLen);
-        }
-        let end = self.ssid_len as usize;
-        match core::str::from_utf8(&self.ssid_buf[..end]) {
+        match self.ssid_.as_str() {
             Ok(ssid) => Ok(ssid),
-            _ => Err(WlanError::Utf8),
+            Err(SerdesError::StrLenTooBig) => Err(WlanError::StrLen),
+            Err(SerdesError::Utf8Decode) => Err(WlanError::Utf8),
         }
     }
 
-    /// Make a string slice for the password (doing it this way helps pacify borrow checker).
+    /// Make a string slice for the password
     pub fn pass(&self) -> Result<&str, WlanError> {
-        if self.pass_len as usize > PASS_BUF_SIZE {
-            // Looks like an out of range string length argument came over the COM bus
-            return Err(WlanError::StrLen);
-        }
-        let end = self.pass_len as usize;
-        match core::str::from_utf8(&self.pass_buf[..end]) {
+        match self.pass_.as_str() {
             Ok(pass) => Ok(pass),
-            _ => Err(WlanError::Utf8),
+            Err(SerdesError::StrLenTooBig) => Err(WlanError::StrLen),
+            Err(SerdesError::Utf8Decode) => Err(WlanError::Utf8),
         }
     }
 }
 
 /// Implement the ComState::WLAN_SET_SSID verb to set the SSID for use by ComState::WLAN_JOIN.
 pub fn set_ssid(ws: &mut WlanState) -> Result<&str, WlanError> {
-    if SSID_COM_RX_WORDS * 2 != ws.ssid_buf.len() + 2 {
+    if ComState::WLAN_SET_SSID.w_words != STR_32_WORDS as u16 {
         return Err(WlanError::Assert); // This should never happen
     }
-    for i in 0..SSID_COM_RX_WORDS {
+    let mut rx_words = [0u16; STR_32_WORDS];
+    for w in rx_words.iter_mut() {
         match com_rx(500) {
-            Ok(rx_word) => match i {
-                0 => ws.ssid_len = rx_word,
-                _ => {
-                    let b = rx_word.to_le_bytes();
-                    let n = (i - 1) * 2;
-                    ws.ssid_buf[n] = b[0];
-                    ws.ssid_buf[n + 1] = b[1];
-                }
-            },
+            Ok(rx) => *w = rx,
             Err(_) => return Err(WlanError::Timeout), // This means COM bus out of sync. VERY BAD.
         }
     }
-    ws.ssid()
+    match ws.ssid_.decode_u16(&rx_words) {
+        Ok(ssid) => Ok(ssid),
+        Err(SerdesError::StrLenTooBig) => Err(WlanError::StrLen),
+        Err(SerdesError::Utf8Decode) => Err(WlanError::Utf8),
+    }
 }
 
 /// Implement the ComState::WLAN_SET_PASS verb to set the password for use by ComState::WLAN_JOIN.
 pub fn set_pass(ws: &mut WlanState) -> Result<&str, WlanError> {
-    if PASS_COM_RX_WORDS * 2 != ws.pass_buf.len() + 2 {
+    if ComState::WLAN_SET_PASS.w_words != STR_64_WORDS as u16 {
         return Err(WlanError::Assert); // This should never happen
     }
-    for i in 0..PASS_COM_RX_WORDS {
+    let mut rx_words = [0u16; STR_64_WORDS];
+    for w in rx_words.iter_mut() {
         match com_rx(500) {
-            Ok(rx_word) => match i {
-                0 => ws.pass_len = rx_word,
-                _ => {
-                    let b = rx_word.to_le_bytes();
-                    let n = (i - 1) * 2;
-                    ws.pass_buf[n] = b[0];
-                    ws.pass_buf[n + 1] = b[1];
-                }
-            },
+            Ok(rx) => *w = rx,
             Err(_) => return Err(WlanError::Timeout), // This means COM bus out of sync. VERY BAD.
         }
     }
-    ws.pass()
+    match ws.pass_.decode_u16(&rx_words) {
+        Ok(pass) => Ok(pass),
+        Err(SerdesError::StrLenTooBig) => Err(WlanError::StrLen),
+        Err(SerdesError::Utf8Decode) => Err(WlanError::Utf8),
+    }
 }
