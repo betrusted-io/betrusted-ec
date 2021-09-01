0488e770e08b29db044fd37ccc18cc6b897f69a8
Sam Blenny <68084116+samblenny@users.noreply.github.com>
2021-09-01 08:02:11 +0000

    Refine rx frame handlers for ARP & IPv4 packets
    Work in progress. This commit is mostly about improving the
    parsing and filtering for ARP and UDP. Gradually working toward
    making a DHCP request and implementing ARP responder (maybe).
    
    New stuff that works:
    - Parsing ARP requests
    - Dropping multicast packets based on MAC (intent is to drop mDNS)
    - Dumping headers for the Ethernet and IP portion of DHCP packets
    
    Speculative / In-progress:
    - Filter to drop frames with fragmented IP packets
    - Function to generate Ethernet MAC header
    
    Signed-off-by: samblenny

--- sw/wfx_rs/src/hal_wf200.rs
+++ sw/wfx_rs/src/hal_wf200.rs
@@ -744,6 +744,32 @@ fn sl_wfx_disconnect_callback(_mac: [u8; 6usize], _reason: u16) {
     // TODO: callback to lwip_set_sta_link_down -- teardown the IP link
 }
 
+// Expected Ethernet frame header sizes
+const MAC_HEADER_LEN: usize = 14;
+const ARP_FRAME_LEN: usize = MAC_HEADER_LEN + 28;
+const IPV4_FRAME_LEN: usize = MAC_HEADER_LEN + 20;
+// Ethertypes for Ethernet MAC header
+const ETHERTYPE_IPV4: &[u8] = &[0x08, 0x00];
+const ETHERTYPE_ARP: &[u8] = &[0x08, 0x06];
+
+// TODO: Expand on this with something to make an ARP request (intent: trigger ARP reply to this MAC)
+fn set_ethernet_mac_header(dest_mac: &[u8; 6], frame: &mut [u8]) -> Result<(), ()> {
+    if frame.len() < MAC_HEADER_LEN {
+        return Err(());
+    }
+    let dest_mac_it = dest_mac.iter();
+    // sl_wfx_context_t.mac_addr_0 is the STA MAC address for the WFx station interface
+    // See https://docs.silabs.com/wifi/wf200/rtos/latest/structsl-wfx-context-t
+    let src_mac = unsafe { WIFI_CONTEXT.mac_addr_0.octet as [u8; 6] };
+    let src_mac_it = src_mac.iter();
+    let ethertype_it = ETHERTYPE_ARP.iter();
+    let mac_header_it = dest_mac_it.chain(src_mac_it).chain(ethertype_it);
+    for (dst, src) in frame.iter_mut().zip(mac_header_it) {
+        *dst = *src;
+    }
+    return Ok(());
+}
+
 fn log_hex(s: &[u8]) {
     for i in s {
         log!(LL::Debug, "{:02X}", *i);
@@ -751,7 +777,11 @@ fn log_hex(s: &[u8]) {
     log!(LL::Debug, " ");
 }
 
-fn log_udp(data: &[u8]) {
+fn handle_ipv4_frame(data: &[u8]) {
+    if data.len() < IPV4_FRAME_LEN {
+        // Drop frames that are too short to hold an IPV4 header
+        return;
+    }
     let dest_mac = &data[..6];
     let src_mac = &data[6..12];
     let ethertype = &data[12..14];
@@ -765,6 +795,21 @@ fn log_udp(data: &[u8]) {
     let ip_checksum = &data[24..26];
     let ip_src = &data[26..30];
     let ip_dst = &data[30..34];
+    const PROTO_UDP: &[u8] = &[0x11];
+    if ip_proto != PROTO_UDP {
+        // Drop frames that are not UDP
+        return;
+    }
+    const IGNORE_DF_MASK: u8 = 0b101_11111;
+    if (ip_flags_frag[0] & IGNORE_DF_MASK != 0) || (ip_flags_frag[1] != 0) {
+        // Drop frames that are part of a fragmented IP packet
+        return;
+    }
+    const VERSION_MASK: u8 = 0xF0;
+    if ip_ver_ihl[0] & VERSION_MASK != 0x40 {
+        // Drop frames with IP version field not equal to 4
+        return;
+    }
     log!(LL::Debug, "RxUDP ");
     log_hex(dest_mac);
     log_hex(src_mac);
@@ -784,39 +829,91 @@ fn log_udp(data: &[u8]) {
     logln!(LL::Debug, "");
 }
 
-fn log_arp(data: &[u8]) {
+/// Handle received Ethernet frame of type ARP (0x0806)
+///
+/// |-------- Ethernet MAC Header --------|----------------------------- ARP --------------------------------------|
+/// | DEST_MAC     SRC_MAC      ETHERTYPE | HTYPE PTYPE HLEN PLEN OPER SHA          SPA      THA          TPA      |
+/// | FFFFFFFFFFFF ------------ 0806      | 0001  0800  06   04   0001 ------------ 0A000101 000000000000 0A000102 |
+/// | ------------ ------------ 0806      | 0001  0800  06   04   0002 ------------ 0A000102 ------------ 0A000101 |
+///
+fn handle_arp_frame(data: &[u8]) {
+    if data.len() < ARP_FRAME_LEN {
+        // Drop malformed (too short) ARP packet
+        return;
+    }
     let dest_mac = &data[..6];
     let src_mac = &data[6..12];
-    let ethertype = &data[12..14];
     log!(LL::Debug, "RxARP ");
     log_hex(dest_mac);
     log_hex(src_mac);
-    log_hex(ethertype);
-    log_hex(&data[14..]);
+    // ARP header for Ethernet + IPv4:
+    //  {htype=0x0001 (Ethernet), ptype=0x0800 (IPv4), hlen=0x06 (6 bytes), plen=0x04 (4 bytes)}
+    const ARP_FOR_ETHERNET_IPV4: &[u8] = &[0, 1, 8, 0, 6, 4];
+    let htype_ptype_hlen_plen = &data[14..20];
+    if htype_ptype_hlen_plen != ARP_FOR_ETHERNET_IPV4 {
+        // Drop ARP packets that do not match the format for IPv4 over Ethernet
+        return;
+    }
+    let arp_oper = &data[20..22];
+    let arp_sha = &data[22..28];
+    let arp_spa = &data[28..32];
+    let arp_tha = &data[32..38];
+    let arp_tpa = &data[38..42];
+    if arp_oper == &[0, 1] {
+        // ARP Request
+        log!(LL::Debug, "who has ");
+        log_hex(arp_tpa);
+        log!(LL::Debug, "tell ");
+        log_hex(arp_sha);
+        log_hex(arp_spa);
+    } else if arp_oper == &[0, 2] {
+        // ARP Reply
+        log_hex(arp_spa);
+        log!(LL::Debug, "is at ");
+        log_hex(arp_sha);
+        log!(LL::Debug, "-> ");
+        log_hex(arp_tha);
+        log_hex(arp_tpa);
+    }
+    if arp_sha != src_mac {
+        // If Ethernet source MAC does not match the ARP sender hardware
+        // address, something weird is happening. Possible that the sending
+        // host has two network interfaces attached to the same LAN?
+        log!(LL::Debug, "WeirdSender");
+    }
     logln!(LL::Debug, "");
 }
 
-fn sl_wfx_host_received_frame_callback(_rx_buffer: *const sl_wfx_received_ind_t) {
-    let body: &sl_wfx_received_ind_body_s = unsafe { &(*_rx_buffer).body };
+fn sl_wfx_host_received_frame_callback(rx_buffer: *const sl_wfx_received_ind_t) {
+    let body: &sl_wfx_received_ind_body_s;
+    unsafe {
+        if rx_buffer.is_null() {
+            logln!(LL::Warn, "WfxRxFr Null");
+            return;
+        }
+        body = &(*rx_buffer).body;
+    }
     let _frame_type: u8 = body.frame_type;
     let padding = body.frame_padding as usize;
     let length = body.frame_length as usize;
     let data = unsafe { &body.frame.as_slice(length + padding)[padding..] };
-    if length < 24 {
-        // Drop frames that are too short
+    const MAC_HEADER_LEN: usize = 14;
+    if length < MAC_HEADER_LEN {
+        // Drop frames that are too short to contain an Ethernet MAC header
+        return;
+    }
+    const MAC_MULTICAST: &[u8] = &[0x01, 0x00, 0x5E, 0x00, 0x00, 0xFB]; // Frequently seen for mDNS
+    let dest_mac = &data[..6];
+    if dest_mac == MAC_MULTICAST {
+        // Drop mDNS
         return;
     }
     let ethertype = &data[12..14]; // ipv4=0x0800, ipv6=0x86DD, arp=0x0806
-    let ip_proto = &data[23..24];
-    const IPV4: &[u8] = &[0x08, 0x00];
-    const ARP: &[u8] = &[0x08, 0x06];
-    const UDP: &[u8] = &[0x11];
-    // Filter packets that are not ARP or IPv4 UDP
-    if ethertype == IPV4 && ip_proto == UDP {
-        log_udp(data);
-    } else if ethertype == ARP {
-        log_arp(data);
-    }
+    match ethertype {
+        ETHERTYPE_IPV4 => handle_ipv4_frame(data),
+        ETHERTYPE_ARP => handle_arp_frame(data),
+        _ => { /* Drop IPv6 and all the rest */ }
+    };
 }
 
 unsafe fn sl_wfx_scan_result_callback(scan_result: *const sl_wfx_scan_result_ind_body_t) {
