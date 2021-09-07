bbc7c5fa491b26871a306caadcb5c52eace6d007
Sam Blenny <68084116+samblenny@users.noreply.github.com>
2021-09-07 11:52:06 +0000

    Improve UDP, DHCP, and ICMP handling
    This is an intermediate step towards implementing a checksum
    function that I'll need for sending packets. Now I can identify
    checksum fields from multicast packets to use as test data.
    
    Changes:
    1. Add ICMP counter as reminder to eventually deal with ICMP
    2. Debug dump UDP headers
    3. Debug dump DHCP headers
    
    This does not parse DHCP options yet.
    
    Signed-off-by: samblenny

--- sw/net/src/filter.rs
+++ sw/net/src/filter.rs
@@ -6,10 +6,11 @@ pub enum FilterBin {
     DropMulti, // Multicast (likely mDNS)
     DropProto, // Unsupported IP layer protocol (perhaps TCP)
     DropFrag,  // Unsupported packet fragment
-    ArpReq,    // ARP request
-    ArpReply,  // ARP reply
-    Dhcp,      // DHCP
-    Udp,       // UDP
+    ArpReq,
+    ArpReply,
+    Icmp,
+    Dhcp,
+    Udp,
 }
 
 /// FilterStats maintains diagnostic statistics on how inbound packets were filtered
@@ -21,6 +22,7 @@ pub struct FilterStats {
     pub drop_frag: u16,
     pub arp_req: u16,
     pub arp_reply: u16,
+    pub icmp: u16,
     pub dhcp: u16,
     pub udp: u16,
 }
@@ -35,6 +37,7 @@ impl FilterStats {
             drop_frag: 0,
             arp_req: 0,
             arp_reply: 0,
+            icmp: 0,
             dhcp: 0,
             udp: 0,
         }
@@ -55,6 +58,7 @@ impl FilterStats {
             FilterBin::DropFrag => self.drop_frag = self.drop_frag.saturating_add(1),
             FilterBin::ArpReq => self.arp_req = self.arp_req.saturating_add(1),
             FilterBin::ArpReply => self.arp_reply = self.arp_reply.saturating_add(1),
+            FilterBin::Icmp => self.icmp = self.icmp.saturating_add(1),
             FilterBin::Dhcp => self.dhcp = self.dhcp.saturating_add(1),
             FilterBin::Udp => self.udp = self.udp.saturating_add(1),
         };
--- sw/net/src/lib.rs
+++ sw/net/src/lib.rs
@@ -6,16 +6,18 @@ use debug::{log, logln, sprint, sprintln, LL};
 pub mod filter;
 use filter::{FilterBin, FilterStats};
 
-// ==========================================================
-// ===== Configure Log Level (used in macro expansions) =====
-// ==========================================================
+// Configure Log Level (used in macro expansions)
 const LOG_LEVEL: LL = LL::Debug;
-// ==========================================================
 
 // Expected Ethernet frame header sizes
 const MAC_HEADER_LEN: usize = 14;
 const ARP_FRAME_LEN: usize = MAC_HEADER_LEN + 28;
-const IPV4_FRAME_LEN: usize = MAC_HEADER_LEN + 20;
+const IPV4_MIN_HEADER_LEN: usize = 20;
+const IPV4_MIN_FRAME_LEN: usize = MAC_HEADER_LEN + IPV4_MIN_HEADER_LEN;
+const UDP_HEADER_LEN: usize = 8;
+const MIN_UDP_FRAME_LEN: usize = IPV4_MIN_FRAME_LEN + UDP_HEADER_LEN;
+const DHCP_HEADER_LEN: usize = 241; // op field -> one byte past options magic cookie
+const MIN_DHCP_FRAME_LEN: usize = MIN_UDP_FRAME_LEN + DHCP_HEADER_LEN;
 
 // Ethertypes for Ethernet MAC header
 const ETHERTYPE_IPV4: &[u8] = &[0x08, 0x00];
@@ -44,13 +46,18 @@ impl NetState {
     pub fn log_state(&self) {
         log!(LL::Debug, "MAC ");
         log_hex(&self.mac);
-        logln!(LL::Debug, "\r\nDropNoise {:X}", self.filter_stats.drop_noise);
+        logln!(
+            LL::Debug,
+            "\r\nDropNoise {:X}",
+            self.filter_stats.drop_noise
+        );
         logln!(LL::Debug, "DropEType {:X}", self.filter_stats.drop_etype);
         logln!(LL::Debug, "DropMulti {:X}", self.filter_stats.drop_multi);
         logln!(LL::Debug, "DropProto {:X}", self.filter_stats.drop_proto);
         logln!(LL::Debug, "DropFrag {:X}", self.filter_stats.drop_frag);
         logln!(LL::Debug, "ArpReq {:X}", self.filter_stats.arp_req);
         logln!(LL::Debug, "ArpReply {:X}", self.filter_stats.arp_reply);
+        logln!(LL::Debug, "Icmp {:X}", self.filter_stats.icmp);
         logln!(LL::Debug, "Dhcp {:X}", self.filter_stats.dhcp);
         logln!(LL::Debug, "Udp {:X}", self.filter_stats.udp);
     }
@@ -82,24 +89,46 @@ pub fn handle_frame(net_state: &mut NetState, data: &[u8]) -> FilterBin {
     return filter_bin;
 }
 
-// TODO: Expand on this with something to make an ARP request (intent: trigger ARP reply to this MAC)
-fn set_ethernet_mac_header(
-    src_mac: &[u8; 6],
-    dst_mac: &[u8; 6],
-    frame: &mut [u8],
-) -> Result<(), ()> {
-    if frame.len() < MAC_HEADER_LEN {
-        return Err(());
-    }
-    let dst_mac_it = dst_mac.iter();
-    let src_mac_it = src_mac.iter();
-    let ethertype_it = ETHERTYPE_ARP.iter();
-    let mac_header_it = dst_mac_it.chain(src_mac_it).chain(ethertype_it);
-    for (dst, src) in frame.iter_mut().zip(mac_header_it) {
-        *dst = *src;
-    }
-    return Ok(());
-}
+/// Populate the MAC header portion of an Ethernet frame buffer
+// fn fill_ethernet_mac_header(
+//     src_mac: &[u8; 6],
+//     dst_mac: &[u8; 6],
+//     frame: &mut [u8],
+// ) -> Result<(), ()> {
+//     if frame.len() < MAC_HEADER_LEN {
+//         return Err(());
+//     }
+//     let dst_mac_it = dst_mac.iter();
+//     let src_mac_it = src_mac.iter();
+//     let ethertype_it = ETHERTYPE_ARP.iter();
+//     let mac_header_it = dst_mac_it.chain(src_mac_it).chain(ethertype_it);
+//     for (dst, src) in frame.iter_mut().zip(mac_header_it) {
+//         *dst = *src;
+//     }
+//     return Ok(());
+// }
+
+/// Populate the IP header portion of an Ethernet frame buffer
+// fn fill_ip_header(
+//     src_ip: &[u8; 4],
+//     dst_ip: &[u8; 4],
+//     protocol: u8,
+//     frame: &mut [u8],
+// ) -> Result<(), ()> {
+//     if frame.len() < IPV4_MIN_FRAME_LEN {
+//         return Err(());
+//     }
+//     let ver_ihl: u8 = 0x4_5; // ver=IPv4, IPv4 Header Length: 5 * 32-bits = 20 bytes
+//     let dcsp_ecn: u8 = 0b000000_00; // Standard service class, Default forwarding, Non ECN-Capable transport
+//     let length: u16 = IPV4_MIN_HEADER_LEN as u16; // Protocol layer will need to update this
+//     let id: u16 = 0xABCD; // TODO: set this with PRNG
+//     let flags_offset: u8 = 0b0_0_0_00000; // Reserved=0, DF=0, MF=0, Offset=0
+//     let ttl: u8 = 0xFF; // Max TTL
+//     let checksum = 0; // Protocol layer needs to update this once length is known; See RFC1071 (checksum)
+
+//     // TODO: Implement this
+//     return Err(());
+// }
 
 fn log_hex(s: &[u8]) {
     for i in s {
@@ -109,13 +138,53 @@ fn log_hex(s: &[u8]) {
 }
 
 fn handle_ipv4_frame(data: &[u8]) -> FilterBin {
-    if data.len() < IPV4_FRAME_LEN {
+    if data.len() < IPV4_MIN_FRAME_LEN {
         // Drop frames that are too short to hold an IPV4 header
         return FilterBin::DropNoise;
+    }
+    let ip_ver_ihl = &data[14..15];
+    let ip_flags_frag = &data[20..22];
+    let ip_proto = &data[23..24];
+    let ip_checksum = &data[24..26];
+    const VER4_LEN5: u8 = 0x4_5;
+    if ip_ver_ihl[0] != VER4_LEN5 {
+        // Drop frames with IP version field not 4 or IP header length longer than minimum (5*32-bits=20 bytes)
+        // DANGER! DANGER! Blithely dropping IP datagrams that have header options is probably a bad idea
+        return FilterBin::DropNoise;
+    }
+    const IGNORE_DF_MASK: u8 = 0b101_11111;
+    if (ip_flags_frag[0] & IGNORE_DF_MASK != 0) || (ip_flags_frag[1] != 0) {
+        // Drop frames that are part of a fragmented IP packet
+        return FilterBin::DropFrag;
+    }
+    //
+    // TODO: Verify checksum
+    //
+    const PROTO_UDP: u8 = 0x11;
+    const PROTO_ICMP: u8 = 0x01;
+    match ip_proto[0] {
+        PROTO_UDP => handle_udp_frame(data),
+        PROTO_ICMP => handle_icmp_frame(data),
+        _ => FilterBin::DropProto,
+    }
+}
+
+fn log_mac_header(data: &[u8]) {
+    if data.len() < MAC_HEADER_LEN {
+        return;
     }
     let dest_mac = &data[..6];
     let src_mac = &data[6..12];
     let ethertype = &data[12..14];
+    log_hex(dest_mac);
+    log_hex(src_mac);
+    log_hex(ethertype);
+}
+
+fn log_ipv4_header(data: &[u8]) {
+    if data.len() < IPV4_MIN_FRAME_LEN {
+        return;
+    }
     let ip_ver_ihl = &data[14..15];
     let ip_dcsp_ecn = &data[15..16];
     let ip_length = &data[16..18];
@@ -126,39 +195,136 @@ fn handle_ipv4_frame(data: &[u8]) -> FilterBin {
     let ip_checksum = &data[24..26];
     let ip_src = &data[26..30];
     let ip_dst = &data[30..34];
-    const PROTO_UDP: &[u8] = &[0x11];
-    if ip_proto != PROTO_UDP {
-        // Drop frames that are not UDP
-        return FilterBin::DropProto;
-    }
-    const IGNORE_DF_MASK: u8 = 0b101_11111;
-    if (ip_flags_frag[0] & IGNORE_DF_MASK != 0) || (ip_flags_frag[1] != 0) {
-        // Drop frames that are part of a fragmented IP packet
-        return FilterBin::DropFrag;
-    }
-    const VERSION_MASK: u8 = 0xF0;
-    if ip_ver_ihl[0] & VERSION_MASK != 0x40 {
-        // Drop frames with IP version field not equal to 4
-        return FilterBin::DropNoise;
-    }
-    log!(LL::Debug, "RxUDP ");
-    log_hex(dest_mac);
-    log_hex(src_mac);
-    log_hex(ethertype);
     log_hex(ip_ver_ihl);
     log_hex(ip_dcsp_ecn);
-    log!(LL::Debug, "len:");
     log_hex(ip_length);
+    log!(LL::Debug, " ");
     log_hex(ip_id);
     log_hex(ip_flags_frag);
+    log!(LL::Debug, " ");
     log_hex(ip_ttl);
-    log!(LL::Debug, "proto:");
     log_hex(ip_proto);
     log_hex(ip_checksum);
+    log!(LL::Debug, " ");
     log_hex(ip_src);
+    log!(LL::Debug, " ");
     log_hex(ip_dst);
+}
+
+fn log_udp_header(data: &[u8]) {
+    if data.len() < MIN_UDP_FRAME_LEN {
+        return;
+    }
+    let udp = &data[IPV4_MIN_FRAME_LEN..];
+    let src_port = &udp[0..2];
+    let dst_port = &udp[2..4];
+    let length = &udp[4..6];
+    let checksum = &udp[6..8];
+    log_hex(src_port);
+    log_hex(dst_port);
+    log!(LL::Debug, " ");
+    log_hex(length);
+    log_hex(checksum);
+}
+
+fn handle_icmp_frame(data: &[u8]) -> FilterBin {
+    if data.len() < IPV4_MIN_FRAME_LEN {
+        return FilterBin::DropNoise;
+    }
+    log!(LL::Debug, "RxICMP ");
+    log_mac_header(data);
+    log!(LL::Debug, "\r\n  ");
+    log_ipv4_header(data);
+    log!(LL::Debug, "\r\n  ");
+    log_hex(&data[IPV4_MIN_FRAME_LEN..]);
     logln!(LL::Debug, "");
-    return FilterBin::Udp;
+    return FilterBin::Icmp;
+}
+
+fn handle_udp_frame(data: &[u8]) -> FilterBin {
+    if data.len() < MIN_UDP_FRAME_LEN {
+        return FilterBin::DropNoise;
+    }
+    // Precondition IP header does not use any options, so is minimum length of 5 words (this is probably a bad idea)
+    let ip_ver_ihl = &data[14..15];
+    if ip_ver_ihl[0] != 0x45 {
+        return FilterBin::DropNoise;
+    }
+    let udp = &data[IPV4_MIN_FRAME_LEN..];
+    let dst_port = &udp[2..4];
+    let _length = &udp[4..6];
+    let _checksum = &udp[6..8];
+    let payload = &udp[8..];
+    match dst_port {
+        &[0, 67] | &[0, 68] => return handle_dhcp_frame(data),
+        _ => {
+            log!(LL::Debug, "RxUDP ");
+            log_mac_header(data);
+            log!(LL::Debug, "\r\n  ");
+            log_ipv4_header(data);
+            log!(LL::Debug, "\r\n  ");
+            log_udp_header(data);
+            log!(LL::Debug, "\r\n  ");
+            log_hex(payload);
+            logln!(LL::Debug, "");
+            return FilterBin::Udp;
+        }
+    };
+}
+
+fn handle_dhcp_frame(data: &[u8]) -> FilterBin {
+    if data.len() < MIN_DHCP_FRAME_LEN {
+        return FilterBin::DropNoise;
+    }
+    let dhcp = &data[MIN_UDP_FRAME_LEN..];
+    let op: &[u8] = &dhcp[0..1];
+    let htype: &[u8] = &dhcp[1..2];
+    let hlen: &[u8] = &dhcp[2..3];
+    let hops: &[u8] = &dhcp[3..4];
+    let xid: &[u8] = &dhcp[4..8];
+    let secs: &[u8] = &dhcp[8..10];
+    let flags: &[u8] = &dhcp[10..12];
+    let ciaddr: &[u8] = &dhcp[12..16];
+    let yiaddr: &[u8] = &dhcp[16..20];
+    let siaddr: &[u8] = &dhcp[20..24];
+    let giaddr: &[u8] = &dhcp[24..28];
+    let chaddr: &[u8] = &dhcp[28..44];
+    let sname: &[u8] = &dhcp[44..108];
+    let file: &[u8] = &dhcp[108..236];
+    let option_mc: &[u8] = &dhcp[236..240]; // Options magic cookie
+    let options: &[u8] = &dhcp[240..]; // First option -> ...
+    log!(LL::Debug, "RxDHCP ");
+    log_mac_header(data);
+    log!(LL::Debug, "\r\n  ");
+    log_ipv4_header(data);
+    log!(LL::Debug, "\r\n  ");
+    log_udp_header(data);
+    log!(LL::Debug, "\r\n  ");
+    log_hex(op);
+    log_hex(htype);
+    log_hex(hlen);
+    log_hex(hops);
+    log!(LL::Debug, " ");
+    log_hex(xid);
+    log!(LL::Debug, " ");
+    log_hex(secs);
+    log_hex(flags);
+    log!(LL::Debug, "\r\n  ");
+    log_hex(ciaddr);
+    log_hex(yiaddr);
+    log_hex(siaddr);
+    log_hex(giaddr);
+    log!(LL::Debug, "\r\n  ");
+    log_hex(chaddr);
+    log!(LL::Debug, "\r\n  ");
+    log_hex(sname);
+    log!(LL::Debug, "\r\n  ");
+    log_hex(file);
+    log!(LL::Debug, "\r\n  ");
+    log_hex(option_mc);
+    log_hex(options);
+    logln!(LL::Debug, "");
+    return FilterBin::Dhcp;
 }
 
 /// Handle received Ethernet frame of type ARP (0x0806)
