bf9442fe3dd72ce4ba821dc8dbe96e413abb7e28
Sam Blenny <68084116+samblenny@users.noreply.github.com>
2021-09-16 12:06:01 +0000

    Improve DHCP options parsing and debug logging
    This steps through the DHCP options block, parsing and logging
    each tag marking the start of an option field, along with its
    length and data. Some of the most common tags are labeled with
    their names. Lengths and data are logged as hex.
    
    The point of better option parsing and logging is to prepare for
    implementing a client to do DHCP discover and renew requests.
    There are many DHCP options, and they can be packed in any order,
    according to the server's choice. So, we need a solid parser to
    interpret whatever the servers decide to reply with.
    
    Signed-off-by: samblenny

--- sw/net/src/dhcp.rs
+++ sw/net/src/dhcp.rs
@@ -1,8 +1,7 @@
-use debug::{log, logln, sprint, sprintln, LL};
-
 use crate::{
     log_hex, log_ipv4_header, log_mac_header, log_udp_header, FilterBin, MIN_UDP_FRAME_LEN,
 };
+use debug::{log, logln, sprint, sprintln, LL};
 
 // Configure Log Level (used in macro expansions)
 const LOG_LEVEL: LL = LL::Debug;
@@ -60,7 +59,146 @@ pub fn handle_dhcp_frame(data: &[u8]) -> FilterBin {
     log_hex(file);
     log!(LL::Debug, "\r\n  ");
     log_hex(option_mc);
-    log_hex(options);
+    handle_all_options(options)
+}
+
+/// Parse the DHCP options field, which is a hot mess.
+///
+/// For option tag meanings see:
+/// - RFC 1533: DHCP Options and BOOTP Vendor Extensions
+/// - RFC 3004: The User Class Option for DHCP
+/// - RFC 1497: BOOTP Vendor Information Extensions
+/// - IETF Draft draft-ietf-wrec-wpad-01 (Web Proxy Auto-Discovery Protocol)
+///
+/// Quick RFC 1533 Summary:
+/// 1. Options 0 and 255 are exceptional because they are fixed length (1 octet long)
+/// 2. All other options are "variable length" and have the format:
+///    | tag_octet | length_octet=n | ... (n octets of data) |
+/// 3. Length octet applies to data, so does not include tag_octet or the length_octet
+/// 4. Some "variable length" fields are actually expected to always be a constant
+///    length, but they are expected to still include a length_octet.
+///
+fn handle_all_options(options: &[u8]) -> FilterBin {
+    let mut options_it = options.iter();
+    // Note that options_it.next() is usually called more than once per iteration of
+    // the for loop, so options.len() is an upper bound (safer than using while-true)
+    let mut end_mode = false;
+    for _ in 0..options.len() {
+        // Log the option's tag
+        if let Some(tag_u8) = options_it.next() {
+            if end_mode {
+                // End mode parsing: log bytes without trying to interpret them as options
+                log!(LL::Debug, " {:X}", tag_u8);
+                continue;
+            } else {
+                // Normal parsing with End & Pad special case checks
+                log!(LL::Debug, "\r\n {:X}", tag_u8);
+                let tag = tag_from(*tag_u8);
+                log_tag_label(tag);
+                match tag {
+                    Tag::End => {
+                        log!(LL::Debug, "\r\n");
+                        end_mode = true;
+                        continue;
+                    }
+                    Tag::Pad => continue,
+                    _ => (),
+                }
+            }
+        } else {
+            break;
+        }
+        // Then log the length and data
+        if let Some(length) = options_it.next() {
+            log!(LL::Debug, " {:X}", length);
+            for _ in 0..(*length as usize) {
+                if let Some(data) = options_it.next() {
+                    log!(LL::Debug, " {:X}", data);
+                }
+            }
+        } else {
+            break;
+        }
+    }
     logln!(LL::Debug, "");
     return FilterBin::Dhcp;
+}
+
+#[derive(Copy, Clone, PartialEq)]
+#[allow(dead_code)]
+enum Tag {
+    Pad,           //   0: length=0 (implicit from RFC; no length octet)
+    End,           // 255: length=0, RFC says subsequent options should be Pads
+    SubnetMask,    //   1: Subnet Mask; length=4
+    GatewayList,   //   3: Gateway IP addresses; length=n*4 for n>=1
+    DnsList,       //   6: DNS servers; length=n*4 for n>=1
+    Hostname,      //  12: Client's hostname
+    DomainName,    //  15: Name of local domain
+    RequestedIp,   //  50: Client's requested IP (DISCOVER); length=4
+    IpLeaseTime,   //  51: Client's requested IP lease time (s) (DISCOVER or REQUEST); length=4 (u32)
+    MsgType,       //  53: One of {1:Discover, 2:Offer, 3:Request, ...}; length=1, range=1-7
+    ServerId,      //  54: IP of server for OFFER and REQUEST (optional for ACK/NACK); length=4
+    ParamReqList,  //  55: Config parameters client wants server to include in reply
+    ErrMsg,        //  56: ASCII error message for use in NACK or DECLINE
+    MaxMsgSize,    //  57: Maximum DHCP message size client is willing to accept; length=2
+    RenewalTime,   //  58: Interval (s) from IP assignment until enter RENEWING; length=4 (u32)
+    RebindingTime, //  59: Interval (s) from IP assignment until enter REBINDING; length=4 (u32)
+    ClassId,       //  60: Clients can use this to inform server of hardware configuration
+    ClientId,      //  61: Unique client ID, normally {hardware_type, MAC_address}
+    UserClass,     //  77: See RFC 3004
+    Wpad,          // 252: WPAD Proxy config URL
+    Other(u8),
+}
+
+const fn tag_from(n: u8) -> Tag {
+    match n {
+        0 => Tag::Pad,
+        255 => Tag::End,
+        1 => Tag::SubnetMask,
+        3 => Tag::GatewayList,
+        6 => Tag::DnsList,
+        12 => Tag::Hostname,
+        15 => Tag::DomainName,
+        50 => Tag::RequestedIp,
+        51 => Tag::IpLeaseTime,
+        53 => Tag::MsgType,
+        54 => Tag::ServerId,
+        55 => Tag::ParamReqList,
+        56 => Tag::ErrMsg,
+        57 => Tag::MaxMsgSize,
+        58 => Tag::RenewalTime,
+        59 => Tag::RebindingTime,
+        60 => Tag::ClassId,
+        61 => Tag::ClientId,
+        77 => Tag::UserClass,
+        252 => Tag::Wpad,
+        n => Tag::Other(n),
+    }
+}
+
+fn log_tag_label(tag: Tag) {
+    let label = match tag {
+        Tag::Pad => &"Pad",
+        Tag::End => &"End",
+        Tag::SubnetMask => &"SubnetMask",
+        Tag::GatewayList => &"GatewayList",
+        Tag::DnsList => &"DnsList",
+        Tag::Hostname => &"Hostname",
+        Tag::DomainName => &"DomainName",
+        Tag::RequestedIp => &"RequestedIP",
+        Tag::IpLeaseTime => &"IpLeaseTime",
+        Tag::MsgType => &"MsgType",
+        Tag::ServerId => &"ServerId",
+        Tag::ParamReqList => &"ParamReqList",
+        Tag::ErrMsg => &"ErrMsg",
+        Tag::MaxMsgSize => &"MaxMsgSize",
+        Tag::RenewalTime => &"RenewalTime",
+        Tag::RebindingTime => &"RebindingTime",
+        Tag::ClassId => &"ClassId",
+        Tag::ClientId => &"ClientId",
+        Tag::UserClass => &"UserClass",
+        Tag::Wpad => &"Wpad",
+        Tag::Other(_) => &"?",
+    };
+    log!(LL::Debug, " ({})", label);
 }
