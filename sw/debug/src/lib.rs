1ef2df48a120b4c93e0b0c25833009d1c1640935
Sam Blenny <68084116+samblenny@users.noreply.github.com>
2021-09-28 00:14:48 +0000

    Increase gateware UART TX FIFO to 256
    This increases the EC gateware UART TX FIFO buffer from 16 bytes
    to 256 bytes so debug logging can send more characters without
    triggering a flow control delay. This should make it less likely
    for debug logging calls to add timing jitter to the EC event loop.
    
    Less timing jitter in the event loop should improve network
    throughput in the presence of error conditions that trigger a debug
    log message. But, most network errors only increment a diagnostic
    counter, so this is just an extra layer of protection.
    
    Impact on logic cell use in nextpnr:
    - Before (16 bytes): `ICESTORM_LC:  4903/ 5280    92%`
    - After (256 bytes): `ICESTORM_LC:  4937/ 5280    93%`
    
    Signed-off-by: samblenny

--- sw/debug/src/lib.rs
+++ sw/debug/src/lib.rs
@@ -35,6 +35,10 @@ impl Uart {
     pub fn putc(c: u8) {
         let mut uart_csr = CSR::new(HW_UART_BASE as *mut u32);
         // Allow TX buffer to drain if it's full
+        // TX buffer is currently 256 bytes (see betrusted-ec/betrusted_ec.py)
+        // Baud rate is 115200 with 8N1. So...
+        // Time to send one byte: 1000ms / (115200 / 10) = 0.087ms
+        // Bytes sent per ms: 1ms / 0.087ms = 11.5
         for _ in 0..FLOW_CONTROL_TIMEOUT_MS {
             if uart_csr.rf(utra::uart::TXFULL_TXFULL) == 1 {
                 delay_ms(1);
